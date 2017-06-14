547c937a18e8010f9376b416a8334f47b3f6745f
andyw324 <andrew.wong@capgemini.com>
2017-06-14 07:43:20 +0100

    test scripts updated

--- runScript.ps1
+++ runScript.ps1
@@ -1,15 +1,15 @@
 ﻿Import-Module .\DeployWindowsServer-functions.psm1
 
+$path = [Environment]::GetFolderPath('UserProfile') + "\Documents\Hyper-V"
 
-
-Deploy-WindowsServer -path [Environment]::GetFolderPath('UserProfile') + "\Documents\Hyper-V" `
+Deploy-WindowsServer -path $path `
                      -autoISO "auto.iso" `
                      -windowsISOpath $path + "\9600.17050.WINBLUE_REFRESH.140317-1640_X64FRE_SERVER_EVAL_EN-US-IR3_SSS_X64FREE_EN-US_DV9.iso" `
-                     -vhdPathArray "@($path + "\hyper-v.vhd")" `
-                     -name = "Hyper-V Server 2012" `
-                     -switch = "vSwitch" `
+                     -vhdPathArray "@('$path\boot.vhd','$path\install.vhd','$path\data.vhd')" `
+                     -name "Hyper-V Server 2012" `
+                     -switch "vSwitch" `
                      -ramSize (2GB) `
                      -vhdSzeArray "@(10GB,10GB,10GB)" `
                      -vhdBlockSizeArray "@(4KB,4KB,64KB)" `
                      -vhdSectorSizeArray "@(512,512,512)" `
-                     -numDrives = 3
+                     -numDrives 3
